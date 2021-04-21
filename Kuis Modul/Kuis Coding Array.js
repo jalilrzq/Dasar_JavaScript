b524111deeb7e0e54ada2f4272e0f65e3b54b9a9
jalilrzq <jalilrzq@gmail.com>
2021-04-22 03:28:12 +0700

    Kuis Coding di Dicoding

---
+++ Kuis Modul/Kuis Coding Array.js
@@ -0,0 +1,20 @@
+/**
+ * TODO:
+ * Buatlah sebuah variabel dengan nama evenNumber yang merupakan sebuah array dengan ketentuan:
+ *   - Array tersebut menampung bilangan genap dari 1 hingga 100
+ *
+ * Catatan:
+ *   - Agar lebih mudah, gunakanlah for loop dan logika if untuk mengisi bilangan genap pada array.
+ */
+
+// TODO
+let evenNumber = [];
+for(let i = 1; i <= 100; i++){
+  if(i%2 === 0){
+    evenNumber.push(i);
+  }
+}
+
+/**
+ * Hiraukan kode di bawah ini
+ */
