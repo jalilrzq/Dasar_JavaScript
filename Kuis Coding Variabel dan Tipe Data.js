0dc1b0b8e5c8a96f2d225af53b2620bbe7d26183
jalilrzq <jalilrzq@gmail.com>
2021-04-22 03:05:43 +0700

    Kuis coding: Variabel dan Tipe Data

---
+++ Kuis Coding Variabel dan Tipe Data.js
@@ -0,0 +1,20 @@
+/**
+ * TODO:
+ * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+ *  - firstName: bertipe data string, dengan nilai nama depan Anda.
+ *  - lastName: bertipe data string, dengan nilai nama belakang Anda.
+ *  - age: bertipe data number, dengan nilai umur Anda.
+ *  - isMarried: bertipe data boolean, dengan bebas Anda tentukan.
+ */
+
+// TODO
+let firstName = "Abdul";
+let lastName = "Jalil";
+let age = 24;
+let isMarried = false;
+
+console.log(`Nama Saya ${firstName} ${lastName}`);
+console.log(`Umur Saya ${age} dan status menikah saya ${isMarried}`);
+/**
+ * Hiraukan kode di bawah ini
+ */
